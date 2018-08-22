08734310ad7b1d601defbb86d290f09ef30f886c
Gabriel Lucena <gnllucena@gmail.com>
2018-08-22 03:02:24 -0300

    Alteração do tema e upgrade para o PowerShell Core.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,8 @@
+# Install-Module posh-git -Scope CurrentUser
+# Install-Module oh-my-posh -Scope CurrentUser
+# https://github.com/oraoto/Jump-Location/releases/tag/0.6.0-ps-core
+# Install-Module PSReadLine -AllowPrerelease -Scope CurrentUser -Force
+
 Import-Module posh-git
 Import-Module oh-my-posh
 Import-Module Jump.Location
@@ -7,4 +12,4 @@ Import-Module PSFzf -ArgumentList 'Ctrl+P','Ctrl+R'
 
 Set-Alias ll Get-ChildItemColor -option AllScope
 Set-Alias ls Get-ChildItemColorFormatWide -option AllScope
-Set-Theme Agnoster
+Set-Theme Paradox
