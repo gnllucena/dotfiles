b8ea233b6f935adae60e6c023115023e1efd272a
Gabriel Lucena <gabriel.lucena@mttechne.com.br>
2019-01-07 13:07:38 -0200

    Alterações na configuração do powershell.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,14 +1,15 @@
 # Install-Module posh-git -Scope CurrentUser
 # Install-Module oh-my-posh -Scope CurrentUser
-# https://github.com/oraoto/Jump-Location/releases/tag/0.6.0-ps-core
+# Install-Module z -Scope CurrentUser
 # Install-Module PSReadLine -AllowPrerelease -Scope CurrentUser -Force
+# Install-Module Get-ChildItemColor -Scope CurrentUser
 
 Import-Module posh-git
 Import-Module oh-my-posh
-Import-Module Jump.Location
+Import-Module z
 Import-Module PSReadLine
 Import-Module Get-ChildItemColor
-Import-Module PSFzf -ArgumentList 'Ctrl+P','Ctrl+R'
+# Import-Module PSFzf -ArgumentList 'Ctrl+P','Ctrl+R'
 
 Set-Alias ll Get-ChildItemColor -option AllScope
 Set-Alias ls Get-ChildItemColorFormatWide -option AllScope
