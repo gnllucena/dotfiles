e3ee1f73583e862f7b6bf818c7e9b0232a49f861
Gabriel Lucena <gnllucena@gmail.com>
2020-08-01 01:46:39 -0300

    Alterações no profile do Powershell

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,15 +2,24 @@
 # Install-Module oh-my-posh -Scope CurrentUser
 # Install-Module z -Scope CurrentUser
 # Install-Module PSReadLine -AllowPrerelease -Scope CurrentUser -Force
+# Install-Module PSReadLine -RequiredVersion 2.0.0-beta3 -AllowPrerelease
 # Install-Module Get-ChildItemColor -Scope CurrentUser -AllowClobber
+# Install-Module PSFzf -Scope CurrentUser
+# scoop install fzf
 
 Import-Module posh-git
 Import-Module oh-my-posh
 Import-Module z
 Import-Module PSReadLine
 Import-Module Get-ChildItemColor
-# Import-Module PSFzf -ArgumentList 'Ctrl+P','Ctrl+R'
+Import-Module PSFzf -ArgumentList 'Ctrl+P','Ctrl+R'
+
+set-psreadlineoption -predictionsource history
+get-psreadlineoption | % predictionsource
 
 Set-Alias ll Get-ChildItemColor -option AllScope
 Set-Alias ls Get-ChildItemColorFormatWide -option AllScope
+Set-Alias .. cd..
 Set-Theme Paradox
+
+cd c:/git
